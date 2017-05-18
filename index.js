35ea677b63e77291193488b0e97d54dc6cf356d1
Colin Eberhardt <colin.eberhardt@gmail.com>
2017-05-18 16:27:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const requestp = require('./requestAsPromise');
+
+const addComment = (url, body, token) =>
+  requestp({
+    json: true,
+    headers: {
+      'Authorization': 'token ' + token,
+      'User-Agent': 'giphybot'
+    },
+    method: 'POST',
+    url,
+    body: {
+      body
+    }
+  });
+
+const searchGifs = (searchTerm) =>
+  requestp({
+    url: 'http://api.giphy.com/v1/gifs/search',
+    json: true,
+    qs: {
+      q: searchTerm,
+      api_key: 'dc6zaTOxFJmzC'
+    }
+  });
+
+const personalAccessToken = process.env.GITHUB_ACCESS_TOKEN;
+
+const regex = /\[gifbot:(.*?)\]/g;
+
+const createGifComment = (searchTerm, gifUrl) => `
+![animated gif of ${searchTerm}](${gifUrl})
+Powered By Giphy and served up by your ever faithful @gifbot
+`;
+
+const noGifComment = (searchTerm) =>
+  `Your faithful @gifbot is sorry to report that it couldn't find a gif for '${searchTerm}'`;
+
+exports.handler = ({ body }, lambdaContext, callback) => {
+  const loggingCallback = (err, message) => {
+    console.log('callback', err, message);
+    callback(err, message);
+  };
+
+  if (body.action !== 'created') {
+    loggingCallback(null, {'message': 'ignored action of type ' + body.action});
+    return;
+  }
+
+  console.log(`Looking for gifbot tokens in comment ${body.issue.url}`);
+
+  const matches = regex.exec(body.comment.body);
+  if (!matches) {
+    loggingCallback(null, `The comment didn't summon the almighty gifbot`);
+    return;
+  }
+
+  const searchTerm = matches[1];
+  searchGifs(searchTerm)
+    .then((results) => {
+      let comment = noGifComment(searchTerm);
+      if (results.data.length) {
+        const gifUrl = results.data[0].images.fixed_height.url;
+        comment = createGifComment(searchTerm, gifUrl);
+      }
+      return addComment(body.issue.comments_url, comment, personalAccessToken);
+    })
+    .then(() => loggingCallback(null, 'added comment'))
+    .catch((err) => {
+      loggingCallback(err.toString());
+    });
+};
